0920a81f5d19da79c472087d34997c9f8e62c742
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-07-08 21:38:31 +0530

    [feat]: housing location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component'
+import { Housinglocation } from '../housinglocation'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  housingLocation: Housinglocation = {
+    id:9999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: 'assets/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false
+  }
 }
